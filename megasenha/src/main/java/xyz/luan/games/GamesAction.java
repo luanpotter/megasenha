297ef345a077e1fa58f52b06743347e24a9674b8
Luan Nico <luanpotter27@gmail.com>
2017-06-16 16:04:28 -0300

    random kind

--- megasenha/src/main/java/xyz/luan/games/GamesAction.java
+++ megasenha/src/main/java/xyz/luan/games/GamesAction.java
@@ -12,12 +12,45 @@ public class GamesAction extends Action<Game> {
         return Collections.singletonList(WordsService.getRandom("v."));
     }
 
+    private static final List<Map.Entry<String, Integer>> probs = new ArrayList<>();
+
+    static {
+        probs.add(new AbstractMap.SimpleEntry<>("s.", 16));
+        probs.add(new AbstractMap.SimpleEntry<>("v.", 8));
+        probs.add(new AbstractMap.SimpleEntry<>("adj.", 8));
+        probs.add(new AbstractMap.SimpleEntry<>("adv.", 4));
+        probs.add(new AbstractMap.SimpleEntry<>("pron.", 4));
+        probs.add(new AbstractMap.SimpleEntry<>("num.", 1));
+    }
+
+    private static final Integer SUM;
+
+    static {
+        int sum = 0;
+        for (Map.Entry<String, Integer> i : probs) {
+            sum += i.getValue();
+        }
+        SUM = sum;
+    }
+
+    private static String randomKind() {
+        double rand = Math.random();
+        for (Map.Entry<String, Integer> entry : probs) {
+            double prob = (double) entry.getValue() / SUM;
+            if (rand <= prob) {
+                return entry.getKey();
+            }
+            rand -= prob;
+        }
+        return probs.get(probs.size() - 1).getKey();
+    }
+
     @GET("megasenha")
     public Set<String> megasenha(Map<String, String> params) {
         int amount = Integer.parseInt(params.get("amount"));
         Set<String> results = new HashSet<>();
         while (results.size() < amount) {
-            String kind = "s."; // TODO random kind
+            String kind = randomKind();
             results.add(WordsService.getRandom(kind));
         }
         return results;
